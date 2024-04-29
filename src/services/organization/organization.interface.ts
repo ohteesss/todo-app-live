82559d4e3d2fdc4109b08e1838144af34a9a12c3
Ojo <oluwatobiojo2911@gmail.com>
2024-04-29 10:15:19 +0100

    feat(organization): created types for all organization input interacting with the organization api

---
+++ src/services/organization/organization.interface.ts
@@ -0,0 +1,10 @@
+export interface OrganizationDetails {
+  image_url: null;
+  description: string;
+  id: string;
+  created_at: string;
+  name: string;
+  owner_id: string;
+  updated_at: string;
+}
+export interface AllUsersOrganizations {}
