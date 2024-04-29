2b14b0c274e20aad38a0a8e929b067f9fe41b6ce
Ojo <oluwatobiojo2911@gmail.com>
2024-04-29 10:16:17 +0100

    feat(ui/button): created a disbled props on the LoginButton

--- src/ui/LoginButton.jsx
+++ src/ui/LoginButton.jsx
@@ -1,11 +1,11 @@
 import { twMerge } from "tailwind-merge";
-function LoginButton({ children, onClick, className }) {
+function LoginButton({ children, onClick, className, disabled }) {
   const baseClassname = twMerge(
     "bg-purple-300 p-2 rounded-lg inline-block hover:bg-purple-400 transition-all duration-300",
     className
   );
   return (
-    <button onClick={onClick} className={baseClassname}>
+    <button onClick={onClick} className={baseClassname} disabled={disabled}>
       {children}
     </button>
   );
