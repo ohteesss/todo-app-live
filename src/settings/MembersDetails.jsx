851a48bcaa878798e21402ca19a33cf83c4bf238
Ojo <oluwatobiojo2911@gmail.com>
2024-04-29 10:18:22 +0100

    feat(remove member): implementing removing memebers from an organization

--- src/settings/MembersDetails.jsx
+++ src/settings/MembersDetails.jsx
@@ -1,7 +1,10 @@
+import { useRemoveMember } from "../featuresHook/useOrganization";
 import ButtonIcon from "../ui/ButtonIcon";
 import DeleteIcon from "../ui/icons/delete";
 
 function MembersDetails({ name = "John Doe", role = "user" }) {
+  const { isRemovingMember, removeMember } = useRemoveMember();
+  const memId = "2f69100c-fe3c-4b6d-a388-8335b424899c";
   return (
     <li className="flex items-center justify-between">
       {" "}
@@ -9,7 +12,7 @@ function MembersDetails({ name = "John Doe", role = "user" }) {
         <p>{name}</p>{" "}
         <span className="text-sm px-2 rounded-full bg-slate-200">{role}</span>
       </div>
-      <ButtonIcon>
+      <ButtonIcon onClick={() => removeMember({ orgId: memId, memberId })}>
         {" "}
         <DeleteIcon />{" "}
       </ButtonIcon>
